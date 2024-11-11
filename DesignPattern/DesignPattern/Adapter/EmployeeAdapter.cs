61d7b0864ec6745acd1f994bcc659fd9f295c0d1
= <=>
2024-11-11 11:30:26 +0100

    added solution

---
+++ DesignPattern/DesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,21 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
+namespace DesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly BillingSystem thirdPartyBillingSystem = new();
+
+        //{"101","John","SE","10000"},
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                employees.Add(new Employee(Int32.Parse(employeesArray[i, 0]), employeesArray[i, 1], employeesArray[i, 2], Int32.Parse(employeesArray[i, 3])));
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(employees);
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Observer/Observer.cs
@@ -0,0 +1,40 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/observer
+
+namespace DesignPattern.Observer
+{
+    public class ConcreteObserver(string userName) : IObserver
+    {
+        // Your code goes here... make sure to add this features:
+        //1. Allow to hold the observer's name (e.g. when user Adam wants to observe the subject)
+        //2. Creating the Observer
+        //3. Registering the Observer with the Subject
+        //4. Removing the Observer from the Subject
+        //5. Observer will get a notification from the Subject using the following Method
+        private string UserName { get; set; } = userName;
+
+        public string GetUserName()
+        {
+            return UserName;
+        }
+
+        public void SetUserName(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public void AddSubscriber(Subject subject)
+        {
+            subject.RegisterObserver(this);
+        }
+
+        public void RemoveSubscriber(Subject subject)
+        {
+            subject.RemoveObserver(this);
+        }
+
+        public void Update(string availability)
+        {
+            Console.WriteLine("Hello " + UserName + ", Product is now " + availability + " on Amazon");
+        }
+    }
+}
